76af405a75549011024146f4c3519c8bdbb6939a
csr632 <632882184@qq.com>
2019-09-16 21:12:10 +0800

    add biz-component demo

--- src/index.js
+++ src/index.js
@@ -1,14 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "@aliwind/component";
+import WindAppLayout from "@aliwind/rc-app-layout";
+import WindConsoleMenu from "@aliwind/rc-console-menu";
+
 import "./styles.css";
 import "@aliwind/component/dist/wind.css";
+
+const menuItems = [
+  {
+    key: "overview",
+    label: "概览"
+  },
+  {
+    key: "lists",
+    label: "列表",
+    items: [
+      {
+        key: "basic-list-1",
+        label: "基础列表1"
+      },
+      {
+        key: "basic-list-2",
+        label: "基础列表2"
+      }
+    ]
+  },
+  {
+    key: "basic-form",
+    label: "基础表单"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <h1>Wind Demo</h1>
-      <Button type="primary">Button works!</Button>
+      <WindAppLayout
+        nav={<WindConsoleMenu items={menuItems} header="Wind Demo" />}
+      >
+        页面内容：
+        <Button type="primary">Button works!</Button>
+      </WindAppLayout>
     </div>
   );
 }
